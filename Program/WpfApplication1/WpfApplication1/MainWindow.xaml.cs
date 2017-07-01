d5235dec3c056a0fde7d431035fd8f4b57e3c26e
Nikolay Hristov <sirkillo0o@gmail.com>
2017-07-01 16:49:28 +0300

    image selection logic

--- Program/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ Program/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Svg2Xaml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,9 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindowLogic : Window
     {
+
+        private DrawingImage _selectedShape;
+
         public MainWindowLogic()
         {
             InitializeComponent();
@@ -30,37 +35,57 @@ namespace WpfApplication1
             
         }
 
-        private void SpawnTile(object sender, RoutedEventArgs e)
+        private void SelectShape(object sender, RoutedEventArgs e)
         {
             int tileType = int.Parse(((Button)sender).Tag.ToString());
-            DrawingImage imageToSpawn = GetImageToSpawn(tileType);
+            _selectedShape = GetImageToSpawn(tileType);
         }
 
         // Don't mind this for now
         private DrawingImage GetImageToSpawn(int tileType)
         {
             DrawingImage image = null;
+            string dir = System.IO.Path.GetDirectoryName(System.IO.Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
 
             switch (tileType)
             {
                 case 1:
-                //D:\Projects\Polyiamonths\Program\WpfApplication1\WpfApplication1\images\tile_1.svg
+                    image = GetSVGFromResources(dir + @"\images\tile_1.svg");
                     break;
 
                 case 2:
+                    image = GetSVGFromResources(dir + @"\images\tile_2.svg");
                     break;
 
                 case 3:
+                    image = GetSVGFromResources(dir + @"\images\tile_3.svg");
                     break;
 
                 case 4:
+                    image = GetSVGFromResources(dir + @"\images\tile_4.svg");
                     break;
 
                 case 5:
+                    image = GetSVGFromResources(dir + @"\images\tile_5.svg");
                     break;
 
                 case 6:
+                    image = GetSVGFromResources(dir + @"\images\tile_6.svg");
                     break;
+            }
+
+            return image;
+        }
+
+        private DrawingImage GetSVGFromResources(string fileName)
+        {
+            DrawingImage image = null;
+            using (FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                image = SvgReader.Load(stream);
+
+                // SvgReaderOptions options = new SvgReaderOptions(...);
+                // DrawingImage image = SvgReader.Load(stream, options);
             }
 
             return image;
